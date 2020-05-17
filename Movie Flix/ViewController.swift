53f2d3ef86a2099f8cf2fd11f87a6ecaa2907a47
Ashish Verma <ashish.verma@ridecell.com>
2020-05-17 11:21:03 +0530

    Initial Commit

---
+++ Movie Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Flix
+//
+//  Created by Ashish Verma on 5/17/20.
+//  Copyright © 2020 Ashish Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
